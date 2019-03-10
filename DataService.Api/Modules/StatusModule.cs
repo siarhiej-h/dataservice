5e89a005a36717dc6097835a44bf2835f9facc6e
iatra <light.drow.777@gmail.com>
2019-03-10 20:43:07 +0100

    Renamed core entity to Item

---
+++ DataService.Api/Modules/StatusModule.cs
@@ -0,0 +1,21 @@
+﻿using DataService.Core;
+using DataService.Core.Entities;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace DataService.Api.Modules
+{
+    public sealed class StatusModule : NancyModule
+    {
+        public StatusModule()
+            : base("api/status")
+        {
+            Post("/", o =>
+            {
+                var item = this.Bind<Item>();
+                System.Console.WriteLine($"{item.Name} : {item.Price}");
+                return HttpStatusCode.OK;
+            });
+        }
+    }
+}
---
+++ DataService.Core/Entities/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataService.Core.Entities
+{
+    public class Item : IEntity<string>
+    {
+        public Item()
+        {
+        }
+
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string GetKey() => Name;
+    }
+}
